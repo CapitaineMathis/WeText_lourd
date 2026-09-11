5fd01996cb9e9d802e6e47c38ab772467b3ffdd3
Mathis Prioreschi-Drouin <mathiscap2110@gmail.com>
2026-09-11 23:01:59 +0200

    starting the project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Terminal.Gui.App;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+
+namespace WeText;
+
+public class WeText
+{
+    static void Main(string[] args)
+    {
+        int terminalWidth;
+        int terminalHeight;
+
+        try
+        {
+            terminalWidth = Console.WindowWidth;
+            terminalHeight = Console.WindowHeight;
+        }
+        catch (IOException)
+        {
+            // Console dimensions unavailable.
+            terminalWidth = 120;
+            terminalHeight = 40;
+        }
+    }
+}
